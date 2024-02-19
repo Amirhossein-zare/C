d0e5d5047ebeccfcddb0ed0d7d4f48198236f5f7
AmirHossein Zare <130905177+Amirhossein-zare@users.noreply.github.com>
2024-02-19 17:13:46 +0330

    1D Array Length
    how to get int from user for 1D arrey

---
+++ Arraye 1D Length/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arraye_1D_Length
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] list1 = new int[5];
+
+            for (int i = 0; i < list1.Length; i++)
+            {
+                Console.Write("Enter Your Numbers: a[{0}]", i+1);
+                list1[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i = 0; i < list1.Length; i++)
+            {
+                list1[i] = i;
+            }
+            for (int i = 0;i < list1.Length; i++)
+            {
+                Console.WriteLine(list1[i]+"\t");
+            }
+        }
+    }
+}
