c56131ae80af63ca4e0095cd62d951a7f52db005
AmirHossein Zare <130905177+Amirhossein-zare@users.noreply.github.com>
2024-02-20 00:31:15 +0330

    1D Array with Char
    how to use Char in 1D array

---
+++ Arreye 1D with Char/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arreye_1D_with_Char
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] Name = { 'h', 'a', 'n', 'i', 'e', 'h' };
+
+            Console.WriteLine(Name.Length);
+            
+            Console.WriteLine("************");
+            
+
+            foreach (var item in Name)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("**********");
+
+            string name = "hanieh";
+            Console.WriteLine(name);
+            Console.WriteLine();
+        }
+    }
+}
