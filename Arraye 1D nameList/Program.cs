c94b1fec6d5045355b436aeb19807be13a08f936
AmirHossein Zare <130905177+Amirhossein-zare@users.noreply.github.com>
2024-02-19 09:17:24 +0330

    Array 1D with nameList
    how to work with 1D array and creat a nameList

---
+++ Arraye 1D nameList/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arraye_1D_nameList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nameList = new string[5];
+            
+            for (int i = 0; i < 5; i++) 
+            {
+                Console.Write("Enter Some Names: ");
+                nameList[i] = Convert.ToString(Console.ReadLine());
+            }
+            foreach (var item in nameList)
+            {
+                Console.Write("{0}",item);
+            }
+            Console.WriteLine();
+        }
+    }
+}
