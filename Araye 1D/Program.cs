19963aacab429c8865ca5c6d4d05bcbc7c10ae97
AmirHossein Zare <130905177+Amirhossein-zare@users.noreply.github.com>
2024-02-19 08:36:57 +0330

    1D Array
    how to create a 1D Array

---
+++ Araye 1D/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Araye_1D
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] list1 = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            Write("Your Lengh is : ");
+
+            for (int i = 0; i < list1.Length; i++)
+            {
+                Console.Write(list1[i]);
+            }
+            foreach (var item in list1)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+    }
+}
