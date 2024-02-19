7e5658c676df6d0141a126f26b1a84b804f8d87b
AmirHossein Zare <130905177+Amirhossein-zare@users.noreply.github.com>
2024-02-20 00:30:22 +0330

    Persian Calender
    how to use persian calender

---
+++ Persian Calender/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Persian_Calender
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PersianCalendar Calendar = new PersianCalendar();
+            
+            int Day = Calendar.GetDayOfMonth(DateTime.Now);
+            int Month = Calendar.GetMonth(DateTime.Now);
+            int Year = Calendar.GetYear(DateTime.Now);
+
+            string Calender = (Year + "/" + Month + "/" + Day);
+            Console.WriteLine(Calender);
+        }
+
+    }
+}
