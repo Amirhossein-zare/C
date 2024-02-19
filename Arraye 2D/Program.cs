f8604b9bb11f54a2856d0f8b63b5d91b7e56d681
AmirHossein Zare <130905177+Amirhossein-zare@users.noreply.github.com>
2024-02-19 09:07:05 +0330

    2D Array
    how to use 2D Array

---
+++ Arraye 2D/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Arraye_2D
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] a = new int[3, 4];
+            
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write("Enter Some Numbers [{0},{1}] : ",i,j);
+                    a[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            Console.WriteLine();
+
+            Console.Clear();
+
+            for (int i = 0;i < 3; i++)
+            {
+                for (int j = 0;j < 4; j++)
+                {
+                    Console.Write(a[i,j]+ " " );
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
