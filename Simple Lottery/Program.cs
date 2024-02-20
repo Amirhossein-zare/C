706c0401cd887ba32097b6b82c858c0124d089ca
AmirHossein Zare <130905177+Amirhossein-zare@users.noreply.github.com>
2024-02-21 00:56:47 +0330

    Simple Lottery
    simple lottery for knowing how to use random class

---
+++ Simple Lottery/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Lottery
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Random rnd = new Random();
+
+            
+            for (int i = 0; i < 20; i++)
+            {
+                int a = rnd.Next(1, 20);
+                Console.WriteLine("Winner: {0}",a);
+            }
+        }
+    }
+}
