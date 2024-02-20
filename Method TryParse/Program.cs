64633baa789631e0811d2ec7e8546934f1318c27
AmirHossein Zare <130905177+Amirhossein-zare@users.noreply.github.com>
2024-02-21 00:54:57 +0330

    Try Parse
    how to use TryParse

---
+++ Method TryParse/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_TryParse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Your Number: ");
+            int number;
+
+            while (!int.TryParse(Console.ReadLine(), out number))
+            {
+                Console.Write("Number Plz Not Other Things -_-\n");
+            }
+            Console.WriteLine();
+        }
+    }
+}
