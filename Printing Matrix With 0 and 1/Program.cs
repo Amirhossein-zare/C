a81b7afdaec750f1b1c16cf3e7117935a5e53e97
AmirHossein Zare <130905177+Amirhossein-zare@users.noreply.github.com>
2024-02-21 02:10:18 +0330

    Printing Matrix
    simple matrix with0,1

---
+++ Printing Matrix With 0 and 1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Printing_Matrix_With_0_and_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter First Number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Clear();
+
+            Console.Write("Enter Second Number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Clear();
+
+            for (int i = 0; i < num1; i++)
+            {
+                for (int j = 0; j < num2; j++)
+                {
+                    Thread.Sleep(50);
+                    if (i == j)
+                    {
+                        Console.Write("1 ");
+                    }
+                    else
+                    {
+                        Console.Write("0 ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
