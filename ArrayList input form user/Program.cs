a3d2a20780cd2165e2d4de5f3eabfa01d9c5a95b
AmirHossein Zare <130905177+Amirhossein-zare@users.noreply.github.com>
2024-02-21 02:08:47 +0330

    Arraylist  From User
    arraylist input from user

---
+++ ArrayList input form user/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayList_input_form_user
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list1 = new ArrayList();
+
+            Console.Write("Enter Your Numbers: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+
+            while (number >= 0) 
+            {
+                count++;
+                list1.Add(number);
+                Console.WriteLine("Your Num: {0}",count);
+            }
+        }
+    }
+}
