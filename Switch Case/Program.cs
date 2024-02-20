51c1d157e7903363430ffdd1148f1839582fa645
AmirHossein Zare <130905177+Amirhossein-zare@users.noreply.github.com>
2024-02-21 00:55:47 +0330

    Switch Case
    how to use switch case

---
+++ Switch Case/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch_Case
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a Character: ");
+            char opr = Convert.ToChar(Console.ReadLine());
+
+            switch (opr)
+            {
+                case '+':
+                    Console.Write("Sum");
+                    break;
+                case '-':
+                    Console.Write("Minus");
+                    break;
+                case '*':
+                    Console.Write("Multiply");
+                    break;
+                case '/':
+                    Console.Write("Divide");
+                    break;
+            }
+            Console.WriteLine();
+        }
+    }
+}
