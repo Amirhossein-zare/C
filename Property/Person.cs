87ed1214edcc220b42d0bc7ab04072b017255d82
AmirHossein Zare <130905177+Amirhossein-zare@users.noreply.github.com>
2024-02-22 15:16:56 +0330

    Property
    how to create property's (1)

---
+++ Property/Person.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonInfo
+{
+    internal class Person
+    {
+        private int personId;
+        private string personName;
+        private string personLastName;
+        private int personAge;
+        private float personAvg;
+
+        public int PersonId
+        {
+            set { personId = value; }
+            get { return personId; }
+        }
+
+        public string PersonName
+        {
+            set { personName = value; }
+            get { return personName; }
+        }
+
+        public string PersonLastName
+        {
+            set { personLastName = value; }
+            get { return personLastName; }
+        }
+
+        public int PersonAge
+        {
+            set { personAge = value; }
+            get { return personAge; }
+        }
+
+        public float PersonAvg
+        {
+            set { personAvg = value; }
+            get { return personAvg; }
+        } 
+
+        public void PersonInfo()
+        {
+            Console.WriteLine("id: {0}\tname: {1}\tLname: {2}\tAge: {3}\tAvg: {4}", personId, personName, personLastName, personAge, personAvg);
+        }
+        /// <summary>
+        /// این متد برای مقدار دهی اعضای خصوصی کلاس استفاده میشود
+        /// </summary>
+        /// <param name="personId">شماره</param>
+        /// <param name="personName">نام</param>
+        /// <param name="personLastName">فامیلی</param>
+        /// <param name="personAge">سن</param>
+        /// <param name="personAvg">میانگین نمرات</param>
+        public void Setter(int personId, string personName, string personLastName, int personAge, float personAvg)
+        {
+            this.personId = personId;
+            this.personName = personName;
+            this.personLastName = personLastName;
+            this.personAge = personAge;
+            this.personAvg = personAvg;
+        }
+        public void Setter(int personId, string personName, string personLastName, int personAge)
+        {
+            this.personId = personId;
+            this.personName = personName;
+            this.personLastName = personLastName;
+            this.personAge = personAge;
+        }
+        public void Setter(int personId, string personName, string personLastName)
+        {
+            this.personId = personId;
+            this.personName = personName;
+            this.personLastName = personLastName;
+        }
+        public void Setter(int personId, string personName)
+        {
+            this.personId = personId;
+            this.personName = personName;
+        }
+        public void Setter(int personId)
+        {
+            this.personId = personId;
+        }
+    }
+}
---
+++ Property/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using PersonInfo;
+
+namespace Property
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+
+            // set
+            person1.PersonId = 1;
+            // get
+            int x = person1.PersonId;
+            //set
+            person1.PersonName = "Amir";
+            person1.PersonLastName = "Zare";
+            person1.PersonAge = 21;
+            person1.PersonAvg = 14.5f;
+            //get
+            var a = person1.PersonName;
+            var b = person1.PersonLastName;
+            var c = person1.PersonAge;
+            var d = person1.PersonAvg;
+            //Wrtitng methods
+            Console.WriteLine(person1.PersonId + " " + person1.PersonName + " " + person1.PersonLastName + " " + person1.PersonAge + " " + person1.PersonAvg + " "); ;
+
+            Console.WriteLine("id: {0}\tname: {1}\tLname: {2}\tAge: {3}\tAvg: {4}"
+                , person1.PersonId, person1.PersonName, person1.PersonLastName, person1.PersonAge, person1.PersonAvg);
+            Console.WriteLine($"id: {person1.PersonId}\tname: {person1.PersonName}\tlname: {person1.PersonLastName}\t" +
+                $"age: {person1.PersonAge}\tavg: {person1.PersonAvg}");
+            person1.PersonInfo();
+
+            person1.Setter(1, "amir", "zare", 21, 19.4f);
+            
+
+        }
+    }
+}
