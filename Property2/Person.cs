06b780bf3a1259efd3f54b8a8cb422fec6284863
AmirHossein Zare <130905177+Amirhossein-zare@users.noreply.github.com>
2024-02-22 15:18:56 +0330

    Property2
    how to create property and how to use get and set

---
+++ Property2/Person.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Info
+{
+    internal class Person
+    {
+        int IdPerson;
+        string NamePerson;
+        string LastName;
+        int AgePerson;
+
+        public Person(){}
+
+        public Person(string NamePerson,string LastName)
+        {
+            this.NamePerson = NamePerson;
+            this.LastName = LastName;
+        }
+        public Person(int IdPerson)
+        {
+            this.IdPerson = IdPerson;
+        }
+        public Person(int IdPerson, string NamePerson)
+        {
+            this.IdPerson = IdPerson;
+            this.NamePerson = NamePerson;
+        }
+        public Person(int IdPerson, string NamePerson, string LastName)
+        {
+            this.IdPerson = IdPerson;
+            this.NamePerson = NamePerson;
+            this.LastName = LastName;
+        }
+
+        public Person(int IdPerson,string NamePerson,string LastName,int AgePerson)
+        {
+            this.IdPerson = IdPerson;
+            this.NamePerson = NamePerson;
+            this.LastName = LastName;
+            this.AgePerson = AgePerson;
+        }
+
+        public void PersonInfo()
+        {
+            Console.WriteLine("id: {0}\tname: {1}\tlname: {2}\tage: {3}", IdPerson, NamePerson, LastName, AgePerson); ;
+        }
+    }
+}
---
+++ Property2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Info;
+
+namespace Property2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person(1,"amir","zare",21);
+
+            Person person2 = new Person();
+
+            
+        }
+    }
+}
