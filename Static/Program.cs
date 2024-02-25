669a6a0e6cc895d90b59b52cf6c8e539123536ff
AmirHossein Zare <130905177+Amirhossein-zare@users.noreply.github.com>
2024-02-26 01:41:08 +0330

    Static
    how to use and learn static

---
+++ Static/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Static;
+
+namespace Static
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StaticClass st1 = new StaticClass();
+
+            st1.Id = 1;
+            st1.Name = "test";
+            st1.Lname = "testeeee";
+            Console.WriteLine(StaticClass.Count);
+
+
+
+            StaticClass st2 = new StaticClass();
+
+            st2.Id = 2;
+            st2.Name = "test1";
+            st2.Lname = "testeeeeeeee";
+            st2.Info();
+
+            
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Static/StaticClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Static
+{
+    internal class StaticClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Lname { get; set; }
+        public static int Count { get; set; } = 0;
+
+        public StaticClass()
+        {
+            Count++;
+        }
+
+        public void Info()
+        {
+            Console.WriteLine("id: {0}\tname: {1}\tlname: {2}||Count is: {3}",Id, Name,Lname,Count);
+        }
+        public static void PName()
+        {
+            Console.WriteLine("web site users: {0}",Count);
+        }
+    }
+}
