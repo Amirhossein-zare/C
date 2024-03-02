d4b9a53321004cd49c6e0f55affe34edfd42473e
AmirHossein Zare <130905177+Amirhossein-zare@users.noreply.github.com>
2024-03-02 23:54:03 +0330

    Clone Method
    how to use clone

---
+++ Clone in ArrayList/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clone_in_ArrayList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list1 = new ArrayList(){  "Amir" , "puria"};
+            ArrayList list2 = (ArrayList)list1.Clone();
+
+
+            foreach (var item in list1)
+            {
+                Console.Write(item);
+            }
+            Console.WriteLine();
+            Console.WriteLine("****************************");
+            foreach (var item in list2)
+            {
+                Console.Write(item);
+            }
+            Console.WriteLine();
+        }
+    }
+}
