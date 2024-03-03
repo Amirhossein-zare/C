b5891cf17f1c7d9d93a91ff0217bc4ca89c03f21
AmirHossein Zare <130905177+Amirhossein-zare@users.noreply.github.com>
2024-03-03 09:23:58 +0330

    While
    how to use while

---
+++ While/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Number :");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int sum = 0;
+            while (num >= 0) 
+            {
+                sum += num;
+                Console.Write("Enter Number :");
+                num = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
