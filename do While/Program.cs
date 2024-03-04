39de093373a6d2210349422c2c0c5e324a8ecc05
AmirHossein Zare <130905177+Amirhossein-zare@users.noreply.github.com>
2024-03-04 17:46:03 +0330

    do while
    how to use do while

---
+++ do While/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace do_While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int number;
+            do
+            {
+                Console.Write("Enter Number:");
+                number = Convert.ToInt32(Console.ReadLine());
+                sum += number;
+
+            }while (number > 0);
+            if (number < 0) 
+            {
+                Console.Write("bad number");
+            }
+            else
+            {
+                Console.WriteLine(sum);
+            }
+            Console.WriteLine();
+        }
+    }
+}
