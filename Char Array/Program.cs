b8de7bf0b6ea46dae99b3a640c2f2f440433ac44
AmirHossein Zare <130905177+Amirhossein-zare@users.noreply.github.com>
2024-03-05 22:17:08 +0330

    Char Arrey
    how to use Arrye with Char

---
+++ Char Array/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Char_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] charArray = { 'a', 'm', 'i', 'r', 'h', 'o', 's', 's', 'i', 'n' };
+
+            Console.Write(charArray.Length);
+            foreach (var item in charArray)
+            {
+                Console.Write(item);
+            }
+            Console.WriteLine();
+
+            string name = "amirhossein";
+            Console.Write(name);
+
+            Console.WriteLine();
+        }
+    }
+}
