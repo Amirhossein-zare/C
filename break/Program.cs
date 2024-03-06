78e6c2fe1e74704a1bee7f52e00c1287e3accbb4
AmirHossein Zare <130905177+Amirhossein-zare@users.noreply.github.com>
2024-03-06 17:05:21 +0330

    Break
    how to use break C#

---
+++ break/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace @break
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 20; i--) 
+            {
+                if (i == -200) 
+                {
+                    break;
+                }
+                Console.Write("Amir"+i);
+                Console.WriteLine();
+                Thread.Sleep(50);
+            }
+        }
+    }
+}
