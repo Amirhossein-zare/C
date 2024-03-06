f80c180946790e1d83a4e0a9d6f98883fe20f7dc
AmirHossein Zare <130905177+Amirhossein-zare@users.noreply.github.com>
2024-03-06 17:06:28 +0330

    Continue
    how to use continue C#

---
+++ continue/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace @continue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 20; i++) 
+            {
+                if (i == 10)
+                {
+                    continue;
+                }
+                Console.Write("Amir"+i);
+                Console.WriteLine();
+                Thread.Sleep(50);
+            }
+        }
+    }
+}
