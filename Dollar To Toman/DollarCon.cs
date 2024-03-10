2673316ece15fec98d45e96a17dfd0df4fb006fe
AmirHossein Zare <130905177+Amirhossein-zare@users.noreply.github.com>
2024-03-10 19:02:17 +0330

    Simple Money Convertor
    dollar to toman(iran)

---
+++ Dollar To Toman/DollarCon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dollar_To_Toman
+{
+    public static class DollarCon
+    {
+        public static int Dollar(this int toman)
+        {
+            toman = toman * 60000;
+
+            return toman;
+        }
+    }
+}
---
+++ Dollar To Toman/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dollar_To_Toman
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Your Money In Dollar: ");
+            float dollar = float.Parse(Console.ReadLine());
+
+            //dollar.Dollar();
+            Console.WriteLine("Your Money In Toman Is : {0}",((int)dollar).Dollar());
+        }
+    }
+}
