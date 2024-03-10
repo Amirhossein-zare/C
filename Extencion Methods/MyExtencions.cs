6f3fdd66ac8c921ddac8d18e8cd9da0f7e167014
AmirHossein Zare <130905177+Amirhossein-zare@users.noreply.github.com>
2024-03-10 19:03:46 +0330

    Extenction Methods
    some of extention methods

---
+++ Extencion Methods/MyExtencions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extencion_Methods
+{
+    static class MyExtencions
+    {
+       public static string ToShamsi(this DateTime date)
+        {
+            PersianCalendar pc = new PersianCalendar();
+           
+            return  pc.GetYear(date) + "/" + pc.GetMonth(date) + "/" + pc.GetDayOfMonth(date);
+        }
+        public static void Print(this string str)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
---
+++ Extencion Methods/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Extencion_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine( DateTime.Now.ToShamsi());
+            string name = "amir";
+            name.Print();
+        }
+    }
+}
