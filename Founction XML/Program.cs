5c2d52d5f789cd367d9c749db955d522046d46cd
AmirHossein Zare <130905177+Amirhossein-zare@users.noreply.github.com>
2024-03-10 09:31:52 +0330

    XML
    how to use xml commends

---
+++ Founction XML/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Founction_XML
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Number1: ");
+            float num1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Enter Number2: ");
+            float num2 = float.Parse(Console.ReadLine());
+
+            Console.Write("Enter Number3: ");
+            float num3 = float.Parse(Console.ReadLine());
+
+            Console.Write(Avg(num1,num2,num3));
+            Console.WriteLine();
+        }
+     /// <summary>
+     /// این کد برای محاسبه 3 تا عدد با میانگین است
+     /// </summary>
+     /// <param name="a">اول</param>
+     /// <param name="b">دوم</param>
+     /// <param name="c">سوم</param>
+     /// <returns></returns>
+    static float Avg(float a , float b, float c)
+    {
+        float sum = a + b + c;
+        return sum / 3;
+    }
+        /// <summary>
+        /// این کد برای محاسبه 2 تا عدد با میانگین است
+        /// </summary>
+        /// <param name="a">اول</param>
+        /// <param name="b">دوم</param>
+        /// <returns></returns>
+    static float Avg(float a , float b)
+    {
+        float sum = a + b;
+        return sum / 2;
+    }
+    }
+}
