7caacaf46104353697a149c16e7490dfa8abbcfd
AmirHossein Zare <130905177+Amirhossein-zare@users.noreply.github.com>
2024-03-10 09:32:26 +0330

    ArrayList Add Method
    how to add method in arraylists

---
+++ ArrayList Add Method/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayList_Add_Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list1 = new ArrayList();
+
+            list1.Add(Console.ReadLine());
+
+            foreach (var item in list1)
+            {
+                while (list1.Count <= 10)
+                {
+                    Console.Write(item);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
