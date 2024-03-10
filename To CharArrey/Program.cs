28807f128bb7264b420db2481555a46425a6e7fe
AmirHossein Zare <130905177+Amirhossein-zare@users.noreply.github.com>
2024-03-10 19:04:53 +0330

    Tochar Array
    how to use tochar array

---
+++ To CharArrey/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace To_CharArrey
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Your Text: ");
+            string text = Console.ReadLine();
+
+            char[] charArrey = text.ToCharArray();
+            foreach (var item in charArrey) 
+            {
+                if (item ==  ' ')
+                    Console.Write("\n");
+                else 
+                    Console.Write(item);
+            }
+            Console.WriteLine();
+
+            string str = new string(charArrey);
+            Console.Write(str);
+
+            Console.WriteLine();
+
+        }
+    }
+}
