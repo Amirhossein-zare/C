f31c4fff0a0d8c0886700205de6dcffe5ce34cfd
AmirHossein Zare <130905177+Amirhossein-zare@users.noreply.github.com>
2024-03-14 02:50:09 +0330

    Throw
    how to use throw

---
+++ Throw/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Throw
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your Avg; ");
+            float avg = float.Parse(Console.ReadLine());
+
+            try
+            {
+                Validate.AvgValid(avg);
+            }
+            catch (MyException ex)
+            {
+                Console.WriteLine("Error : {0}",ex.Message);
+            }
+        }
+    }
+}
---
+++ Throw/Validate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Throw
+{
+    internal class Validate
+    {
+        public static void AvgValid(float avg)
+        {
+            if (avg >= 0 && avg <=20) 
+            {
+                Console.WriteLine("Avg is Valid");
+            }
+            else
+            {
+                throw new MyException("Avg is Not Valid");
+            }
+        }
+    }
+}
