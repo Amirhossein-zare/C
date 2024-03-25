43373ed738230f13651dba0a1e00014a8c90e8e0
AmirHossein Zare <130905177+Amirhossein-zare@users.noreply.github.com>
2024-03-26 00:53:50 +0330

    Goto
    how to use goto

---
+++ Goto/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Goto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        GetNumber:
+            Console.Write("Enter Number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            goto GetNumber;
+        }
+    }
+}
